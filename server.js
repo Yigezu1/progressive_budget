15f6bea6a1c5f32cb17be2d44bb4d5f2ca1dc94f
Yigezu Mulugeta <Yigezumb@gmail.com>
2020-12-19 16:57:53 -0500

    added code to take care of environment

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+require('./db/mongoose');
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,10 +16,10 @@ app.use(express.json());
 
 app.use(express.static("client"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
