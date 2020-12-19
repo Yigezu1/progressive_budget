825d8dec5b33514e5c44393b48a20a18270ec456
Yigezu Mulugeta <Yigezumb@gmail.com>
2020-12-19 17:20:14 -0500

    updated connection

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
-// const mongoose = require("mongoose");
-require('./db/mongoose');
+const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
@@ -16,10 +15,15 @@ app.use(express.json());
 
 app.use(express.static("client"));
 
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget", 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+  );
 
 // routes
 app.use(require("./routes/api.js"));
